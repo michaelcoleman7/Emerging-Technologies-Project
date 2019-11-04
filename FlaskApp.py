71db00b611408c520145c4838f3e8dbaefdf3762
Michael Coleman <michaelcoleman135@gmail.com>
2019-11-04 22:52:45 +0000

    Added code to recieve image data from html via AJAX request

--- FlaskApp.py
+++ FlaskApp.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from PIL import Image
+from io import BytesIO
+import re, base64
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
@@ -7,6 +11,21 @@ app = Flask(__name__)
 def webpage():
     # Render the HTML page Webpage.html from the templates folder
     return render_template('/Webpage.html')
+
+# Map Route to get image data from canvas
+@app.route('/image', methods=['POST'])
+def getImage():
+    # Convert base64 string to image - Adapted from https://github.com/python-pillow/Pillow/issues/3400
+
+    # Request image from html in base64 format
+    image_b64 = request.values['imageBase64']
+    base64_data = re.sub('^data:image/.+;base64,', '', image_b64)
+    # Decode from base64
+    byte_data = base64.b64decode(base64_data) 
+    image_data = BytesIO(byte_data)
+    # Convert to PIL Image
+    img = Image.open(image_data)
+    return ''
 
 if __name__ == '__main__':
     # Run the application 
