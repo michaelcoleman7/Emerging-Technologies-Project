e550279f3b81be258e04181b772a697b87055156
Michael Coleman <michaelcoleman135@gmail.com>
2019-11-15 18:19:46 +0000

    Changed Epochs to 10 in model

--- Web Application/Model.py
+++ Web Application/Model.py
@@ -1,5 +1,6 @@
 # Imports
 import keras as kr
+from keras.layers import Dense, Dropout, Flatten
 import matplotlib.pyplot as plt
 import numpy as np
 import gzip
@@ -19,12 +20,18 @@ def createModel():
     # Build the graph.
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-    #Read in MNIST Data from dataset in folder /data
+    #Read in all MNIST Data from dataset in folder /data , for training and test images
     with gzip.open('MNIST Data Files/train-images-idx3-ubyte.gz', 'rb') as f:
         train_img = f.read()
 
     with gzip.open('MNIST Data Files/train-labels-idx1-ubyte.gz', 'rb') as f:
         train_lbl = f.read()
+
+    with gzip.open('MNIST Data Files/t10k-images-idx3-ubyte.gz', 'rb') as f:
+        test_img = f.read()
+
+    with gzip.open('MNIST Data Files/t10k-labels-idx1-ubyte.gz', 'rb') as f:
+        test_lbl = f.read()
         
     # Initialize train images and labels
     train_img = ~np.array(list(train_img[16:])).reshape(60000, 28, 28).astype(np.uint8) / 255.0
@@ -38,25 +45,12 @@ def createModel():
     encoder.fit(train_lbl)
     outputs = encoder.transform(train_lbl)
 
-    model.fit(inputs, outputs, epochs=2, batch_size=100)
-
-    with gzip.open('MNIST Data Files/t10k-images-idx3-ubyte.gz', 'rb') as f:
-        test_img = f.read()
-
-    with gzip.open('MNIST Data Files/t10k-labels-idx1-ubyte.gz', 'rb') as f:
-        test_lbl = f.read()
+    model.fit(inputs, outputs, epochs=10, batch_size=100)
         
     test_img = ~np.array(list(test_img[16:])).reshape(10000, 784).astype(np.uint8) / 255.0
     test_lbl =  np.array(list(test_lbl[ 8:])).astype(np.uint8)
 
     (encoder.inverse_transform(model.predict(test_img)) == test_lbl).sum()
-
-    # Get model to make a prediction
-    model.predict(test_img[5:6])
-
-    # Test if image is showing on plot
-    plt.imshow(test_img[5].reshape(28, 28), cmap='gray')
-    # plt.show()
 
     # Test the model score and accuracy
     score = model.evaluate(inputs, outputs, verbose=0)
